38bb0b8375ae3cea426eb571cb4a52818783f83c
cyberpompier <sebastien.dupressoir@icloud.com>
2025-02-24 15:24:37 +0100

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Habillement App',
+        short_name: 'Habillement',
+        description: 'A PWA for habillement management.',
+        theme_color: '#007AFF',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+      },
+    })
+  ],
 })
